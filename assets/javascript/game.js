c27d22971c97423068b81fc76aa3e61070150da8
James Roth <jkltroth@gmail.com>
2017-10-09 19:12:44 -0400

    Psuedocoding and html structure complete

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+// PSUEDOCODE
+// // // // // //
+
+// On page load:
+// // 1. Pick a random number for player to guess (between 19 and 120) // Function
+// // 2. Each crystal is assigned a random number between 1-12 // Function
+// // 3. Wins, losses, and total score all start at 0
+
+// Playing the game:
+// // 1. Player clicks on images of crystals
+// // 2. Player's total score goes up based on random number assigned to that crystal
+// // 3. Player wins if total score matches random number
+// // 4. Player loses if total score is greater than random number
+
+// If player wins:
+// // 1. "Wins" counter is increased by 1
+// // 2. "You won!" is displayed
+// // 3. A new random number for player to guess is picked (between 19 and 120) // Function
+// // 4. Crystals are reassigned a random number between 1-12 // Function
+// // 5. Total score is reset to zero // Function
+
+// If player loses:
+// // 1. "Losses" counter is increased by 1
+// // 2. "You lost!" is displayed
+// // 3. A new random number for player to guess is picked (between 19 and 120) // Function
+// // 4. Crystals are reassigned a random number between 1-12 // Function
+// // 5. Total score is reset to zero // Function
+
+// // // // // // // // // // // // // // // // // // // // // // // // // // // 
+
