c36f6671c7f469230cbb734fa99eaeae9862778b
James Roth <jkltroth@gmail.com>
2017-10-09 22:01:19 -0400

    Random number to guess and random values for crystals work but are not unique. On click works and increases total score based on value of image.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -28,3 +28,66 @@
 
 // // // // // // // // // // // // // // // // // // // // // // // // // // // 
 
+$(document).ready(function () {
+
+    var wins = 0;
+    var losses = 0;
+    var totalScore = 0;
+    var numberToGuess;
+    var crystalValue1;
+    var crystalValue2;
+    var crystalValue3;
+    var crystalValue4;
+
+    //function to randomly choose a number to guess between 19 and 120
+    function randomNumber() {
+        min = Math.ceil(19);
+        max = Math.floor(120);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    //function to randomly choose crystal value between 1 and 12    
+    function randomValue() {
+        min = Math.ceil(1);
+        max = Math.floor(12);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    function newGame() {
+        // assigns a random number to guess to the numberToGuess variable
+        numberToGuess = randomNumber();
+
+        //assigns a random value to each of the crystalValue# variables
+        crystalValue1 = randomValue();
+        crystalValue2 = randomValue();
+        crystalValue3 = randomValue();
+        crystalValue4 = randomValue();
+
+        //  Each crystal image is given a data attribute called data-crystalValue set to the value of the crystalValue# variables above.
+        $("#crystal_1").attr("data-crystalvalue", crystalValue1);
+        $("#crystal_2").attr("data-crystalvalue", crystalValue2);
+        $("#crystal_3").attr("data-crystalvalue", crystalValue3);
+        $("#crystal_4").attr("data-crystalvalue", crystalValue4);
+
+        // Applies values for numberToGuess, totalScore, wins, and losses to html
+        $("#randomNumber").text(numberToGuess);
+        $("#totalScore").text(totalScore);
+        $("#wins").text(wins);
+        $("#losses").text(losses);
+    };
+
+    newGame();
+
+    console.log(numberToGuess);
+
+    $(".crystalButton").on("click", function () {
+
+        var crystalValue = ($(this).attr("data-crystalvalue"));
+        crystalValue = parseInt(crystalValue);
+
+        totalScore += crystalValue;
+        $("#totalScore").text(totalScore);
+    });
+
+
+});
