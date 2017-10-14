50429aec8637da52f6fb310a16271dd29d9e31e7
James Roth <jkltroth@gmail.com>
2017-10-14 10:04:27 -0400

    javascript functions correctly. initial CSS styilization created

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -28,66 +28,105 @@
 
 // // // // // // // // // // // // // // // // // // // // // // // // // // // 
 
-$(document).ready(function () {
+var wins = 0;
+var losses = 0;
+var totalScore = 0;
+var numberToGuess;
+var crystalValue1;
+var crystalValue2;
+var crystalValue3;
+var crystalValue4;
 
-    var wins = 0;
-    var losses = 0;
-    var totalScore = 0;
-    var numberToGuess;
-    var crystalValue1;
-    var crystalValue2;
-    var crystalValue3;
-    var crystalValue4;
+//function to randomly choose a number to guess between 19 and 120
+function randomNumber() {
+    min = Math.ceil(19);
+    max = Math.floor(120);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-    //function to randomly choose a number to guess between 19 and 120
-    function randomNumber() {
-        min = Math.ceil(19);
-        max = Math.floor(120);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    };
+//function to randomly choose crystal value between 1 and 12    
+function randomValue() {
+    min = Math.ceil(1);
+    max = Math.floor(12);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-    //function to randomly choose crystal value between 1 and 12    
-    function randomValue() {
-        min = Math.ceil(1);
-        max = Math.floor(12);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    };
+// Function for a new  of play
+function newRound() {
 
-    function newGame() {
-        // assigns a random number to guess to the numberToGuess variable
-        numberToGuess = randomNumber();
+    // assigns a random number to guess to the numberToGuess variable
+    numberToGuess = randomNumber();
 
-        //assigns a random value to each of the crystalValue# variables
-        crystalValue1 = randomValue();
-        crystalValue2 = randomValue();
-        crystalValue3 = randomValue();
-        crystalValue4 = randomValue();
+    //assigns a random value to each of the crystalValue# variables
+    crystalValue1 = randomValue();
+    crystalValue2 = randomValue();
+    crystalValue3 = randomValue();
+    crystalValue4 = randomValue();
 
-        //  Each crystal image is given a data attribute called data-crystalValue set to the value of the crystalValue# variables above.
-        $("#crystal_1").attr("data-crystalvalue", crystalValue1);
-        $("#crystal_2").attr("data-crystalvalue", crystalValue2);
-        $("#crystal_3").attr("data-crystalvalue", crystalValue3);
-        $("#crystal_4").attr("data-crystalvalue", crystalValue4);
+    // Total score is set to 0
+    totalScore = 0;
 
-        // Applies values for numberToGuess, totalScore, wins, and losses to html
-        $("#randomNumber").text(numberToGuess);
-        $("#totalScore").text(totalScore);
-        $("#wins").text(wins);
-        $("#losses").text(losses);
-    };
+    //  Each crystal image is given a data attribute called data-crystalValue set to the value of the crystalValue# variables above.
+    $("#crystal_1").attr("data-crystalvalue", crystalValue1);
+    $("#crystal_2").attr("data-crystalvalue", crystalValue2);
+    $("#crystal_3").attr("data-crystalvalue", crystalValue3);
+    $("#crystal_4").attr("data-crystalvalue", crystalValue4);
 
-    newGame();
+    // Updates values for numberToGuess, totalScore, wins, and losses in html
+    $("#numberToGuess").text(numberToGuess);
+    $("#totalScore").text(totalScore);
+    $("#wins").text(wins);
+    $("#losses").text(losses);
 
     console.log(numberToGuess);
 
-    $(".crystalButton").on("click", function () {
+};
 
-        var crystalValue = ($(this).attr("data-crystalvalue"));
-        crystalValue = parseInt(crystalValue);
+// When the document is ready...
+$(document).ready(function () {
 
-        totalScore += crystalValue;
-        $("#totalScore").text(totalScore);
-    });
+    // Run the new game function
+    newRound();
 
+});
+
+// When a crystal button is clicked...
+$(".crystalButton").on("click", function () {
+
+    // The clickedCrystalValue variable is given the value of the clicked crystal
+    let clickedCrystalValue = ($(this).attr("data-crystalvalue"));
+    // The value of the clickedCrystalValue variable is coverted to an integer
+    clickedCrystalValue = parseInt(clickedCrystalValue);
+
+    // The value of the clickedCrystalValue variable is added to the totalScore variable
+    totalScore += clickedCrystalValue;
+    // totalScore is pushed to the totalScore id element
+    $("#totalScore").text(totalScore);
+
+    // If the total score ever equals the number to guess...
+    if (totalScore === numberToGuess) {
+
+        // Increase wins by one
+        wins++;
+
+        // Updates winOrLoss id in html to display "You won!"
+        $("#winOrLoss").text("You won!");
+
+        //Run the new game function
+        newRound();
+
+    // Else if the total score is ever greater than the number to guess... 
+    } else if (totalScore > numberToGuess) {
+
+        // Increase losses by one
+        losses++;
+
+        // Updates winOrLoss id in html to display "You lost..."
+        $("#winOrLoss").text("You lost...");
+
+        // Run the new game function
+        newRound();
+
+    };
 
 });
